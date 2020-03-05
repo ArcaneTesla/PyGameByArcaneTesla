77ae40dde4e6963e1b8964169fa98c17a428cb86
ArcaneTesla <Agronaut73@gmai.com>
2020-03-05 21:20:17 +1000

    начало создания меню ивентов

--- Game.py
+++ Game.py
@@ -18,12 +18,16 @@ sprit = pygame.image.load('sprite/1.png')  # Подвижная картинка
 sprite = pygame.transform.scale(sprit, (sprit.get_width() * 3, sprit.get_height() * 3))
 spaceship = pygame.image.load('sprite/ship.png')
 pygame.mixer.init()
-pygame.mixer.music.load("Test.mp3")
+pygame.mixer.music.load("Dreamstate Logic - Earthbound.mp3")
 pygame.mixer.music.play()
-
+pygame.font.SysFont('arial', 36)
+f1 = pygame.font.Font(None, 70)
+text1 = f1.render('Ничего не происходит', 1, (255, 255, 255))
+text2 = 1
+iteractionFlag = False
 
 # Начальные координаты объектов
-
+i = 1
 spriteX = 474
 spriteY = 614
 spaceship_x = 200
@@ -52,12 +56,13 @@ barrier3.fill((0, 0, 0))
 barrier3Rect = pygame.Rect(1676, 579, barrier3.get_width(), barrier3.get_height())
 barriers.append((barrier3, barrier3Rect))
 
+interaction = pygame.Surface((96,96))
+interaction.fill((255,0,0))
+interactionRect = pygame.Rect(1570, 645, interaction.get_width(), interaction.get_height())
 
 def newPosition(key_pressed, spriteX, spriteY):
     # Функция пересчитывает координаты новой позиции игрока и проверяет столкновения с блоками
     global myStep
-    global isJump
-    global jumpCount
 
     if key_pressed[pygame.K_LEFT]:
             spriteX -= myStep
@@ -65,27 +70,28 @@ def newPosition(key_pressed, spriteX, spriteY):
             spriteX += myStep
     return spriteX, spriteY
 
-def collisionDetected():
+'''def collisionDetected():
     global blocks
     global spriteRectNew
     colFlag = False
     # Проверка столкновений со всеми блоками в массиве блоков
     for block in blocks:
         if spriteRectNew.colliderect(block[1]):
-
             colFlag = True
-    return colFlag
+    return colFlag'''
 
 def draw_Window():
 
     window.blit(background, (0, 0))
     window.blit(spaceshipScale, (spaceship_x, spaceship_y))
-
+    window.blit(interaction, (1570, 614))
+    window.blit(text1, (100, 50))
     for barrier in barriers:
         window.blit(barrier[0], (barrier[1].x, barrier[1].y))
 
     window.blit(sprite, (spriteRect.x, spriteRect.y))
     pygame.display.update()
+
 
 # Цикл игры
 MainCycle = True
@@ -114,6 +120,14 @@ while MainCycle:
         if spriteRectNew.colliderect(barrier[1]):
             (spriteX, spriteY) = oldPos
             print('Колизия')#Колизия
+
+    if spriteRectNew.colliderect(interactionRect) and key_pressed[pygame.K_e]:
+        text1 = f1.render('Красный квадрат нажат', 1, (255, 255, 255))
+        iteractionFlag = True
+    elif spriteRectNew.colliderect(interactionRect):
+        text1 = f1.render('Нажмите E', 1, (255, 255, 255))
+    else:
+        text1 = f1.render('Ничего не происходит', 1, (255, 255, 255))
 
     draw_Window()# Рисуем всё на экране
 
