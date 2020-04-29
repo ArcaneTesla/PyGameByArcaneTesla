ddec2f6686a2863a49ebe9d8b7b3f299c13a0ce3
ArcaneTesla <Agronaut73@gmai.com>
2020-04-30 01:03:58 +1000

    Интерфейс элементы (пытаюсь)

--- Game.py
+++ Game.py
@@ -1,36 +1,48 @@
 import pygame, sys, time
 FPS = 60  # кадров в сек
 
-# Переменные
-direction = False
-isJump = False
-iteractionFlag = False
-jumpCount = 4
-FPS = 60
-myStep = 20
-OxStatus = 100
-CorpStatus = 100
-ReactorStatus = 100
-
 pygame.init()
 
+#Переменные для сокращения кода
 fpsClock = pygame.time.Clock()
 window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-background = pygame.image.load('sprite/BG.png')  # Фон
-sprit = pygame.image.load('sprite/1.png')  # Подвижная картинка
-sprite = pygame.transform.scale(sprit, (sprit.get_width() * 3, sprit.get_height() * 3))
-spaceship = pygame.image.load('sprite/ship.png')
-HUD = pygame.image.load('sprite/HUD.png')
+# Переменные движения
+direction = False
+iteractionFlag = False
+myStep = 20
+
+#Игровые Переменные
+FPS = 60
+OxStatus = 100
+CorpStatus = 100
+ReactorStatus = 100
+frametime = 0
+background_alpha = 255
+BackAnim = True
+
+#Переменные спрайтов
+background = pygame.image.load('sprite/BG.png').convert()
+background2 = pygame.image.load('sprite/BG2.png').convert()
+sprite = pygame.image.load('sprite/1.png').convert()  # Подвижная картинка
+spaceship = pygame.image.load('sprite/ship.png').convert()
+HUD = pygame.image.load('sprite/HUD.png').convert()
+icon = pygame.image.load('icon.png').convert()
+quest = pygame.image.load('sprite/quest.png').convert()
+
+#Музыка
 pygame.mixer.init()
 pygame.mixer.music.load("Dreamstate Logic - Earthbound.mp3")
 pygame.mixer.music.play()
-pygame.font.SysFont('arial', 36)
-f1 = pygame.font.Font(None, 80)
-text1 = f1.render('Ничего не происходит', 1, (255, 255, 255))
-CorpStatusText = f1.render(str(CorpStatus), 1, (255, 255, 255))
-ReactorStatusText = f1.render(str(ReactorStatus), 1, (255, 255, 255))
-quest = pygame.image.load('sprite/quest.png')
+
+#Игровой Текст
+pygame.font.SysFont('arial', 14)
+f1 = pygame.font.Font(None, 48)
+f2 = pygame.font.Font(None, 82)
+text1 = f1.render('Колизия не обнаружена', 1, (255, 255, 255))
+CorpStatusText = f2.render(str(CorpStatus), 1, (255, 255, 255))
+ReactorStatusText = f2.render(str(ReactorStatus), 1, (255, 255, 255))
+OxStatusText = f2.render(str(OxStatus), 1, (255, 255, 255))
 KeyStatus = False
 
 # Начальные координаты объектов
@@ -39,13 +51,8 @@ spriteX = 474
 spriteY = 614
 spaceship_x = 200
 spaceship_y = 300
-spaceship_widht = 525
-spaceship_height = 173
-
-spaceshipScale = pygame.transform.scale(spaceship, (spaceship_widht * 3, spaceship_height * 3))
 
 # барьерные блоки
-
 barriers = []
 
 barrier1 = pygame.Surface((12, 270))
@@ -53,42 +60,17 @@ barrier1.set_alpha(1)
 barrier1Rect = pygame.Rect(455, 455, barrier1.get_width(), barrier1.get_height())
 barriers.append((barrier1, barrier1Rect))
 
-barrier2 = pygame.Surface((1230, 12))
+barrier2 = pygame.Surface((12, 150))
 barrier2.set_alpha(1)
-barrier2Rect = pygame.Rect(455, 717, barrier2.get_width(), barrier2.get_height())
+barrier2Rect = pygame.Rect(1676, 579, barrier2.get_width(), barrier2.get_height())
 barriers.append((barrier2, barrier2Rect))
 
-barrier3 = pygame.Surface((12, 150))
-barrier3.set_alpha(1)
-barrier3Rect = pygame.Rect(1676, 579, barrier3.get_width(), barrier3.get_height())
-barriers.append((barrier3, barrier3Rect))
-
-interaction = pygame.Surface((96,96))
-interaction.fill((255,0,0))
+#блоки для взаимодействий
+interaction = pygame.image.load('sprite/Chair_active.png').convert()
 interactionRect = pygame.Rect(1570, 645, interaction.get_width(), interaction.get_height())
 
-class Quest():
-
-    def __init__(self, x, y, text, option1, option2, option3):
-        self.x = x
-        self.y = y
-        self.text = text
-        self.option1 = option1
-        self.option2 = option2
-        self.option3 = option3
-
-    '''def draw(self):
-        global iteractionFlag
-        if iteractionFlag == True:
-            window.blit(quest, (200, 200))'''
-
-
-
-
-
-
+#Функция проверяет новую позицию
 def newPosition(key_pressed, spriteX, spriteY):
-    # Функция пересчитывает координаты новой позиции игрока и проверяет столкновения с блоками
     global myStep
 
     if key_pressed[pygame.K_LEFT]:
@@ -97,25 +79,34 @@ def newPosition(key_pressed, spriteX, spriteY):
             spriteX += myStep
     return spriteX, spriteY
 
-'''def collisionDetected():
-    global blocks
-    global spriteRectNew
-    colFlag = False
-    # Проверка столкновений со всеми блоками в массиве блоков
-    for block in blocks:
-        if spriteRectNew.colliderect(block[1]):
-            colFlag = True
-    return colFlag'''
-
+#функция отрисовки
 def draw_Window():
 
     global iteractionFlag
+    global key_pressed
+    global OxStatusText
+    global frametime
+    global background_alpha
+    global BackAnim
 
+    if BackAnim == True:
+        background_alpha -= 5
+        if background_alpha <= 100:
+            BackAnim = False
+    elif BackAnim == False:
+        background_alpha += 5
+        if background_alpha >= 255:
+            BackAnim = True
+
+    background.set_alpha(background_alpha)
+    window.blit(background2, (0, 0))
     window.blit(background, (0, 0))
     window.blit(HUD, (0, 0))
-    window.blit(spaceshipScale, (spaceship_x, spaceship_y))
-    window.blit(interaction, (1570, 614))
-    window.blit(text1, (600, 50))
+    window.blit(spaceship, (spaceship_x, spaceship_y))
+    window.blit(text1, (20, 900))
+    if spriteRectNew.colliderect(interactionRect):
+        window.blit(interaction, (1574, 642))
+
     window.blit(sprite, (spriteRect.x, spriteRect.y))
     window.blit(OxStatusText, (660, 1000))
     window.blit(CorpStatusText, (875, 1000))
@@ -124,15 +115,14 @@ def draw_Window():
     if iteractionFlag == True:
         window.blit(quest, (200, 200))
 
-    for barrier in barriers:
-        window.blit(barrier[0], (barrier[1].x, barrier[1].y))
-
+    pygame.display.set_icon(icon)
     pygame.display.update()
+
 
 # Цикл игры
 MainCycle = True
 while MainCycle:
-    fpsClock.tick(FPS) # Частота обновления экрана
+    fpsClock.tick(30) # Частота обновления экрана
     key_pressed = pygame.key.get_pressed()
 
     for event in pygame.event.get():
@@ -142,11 +132,6 @@ while MainCycle:
         if key_pressed[pygame.K_ESCAPE]:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.KEYDOWN:
-            KeyStatus = event.key
-        if event.type == pygame.KEYUP:
-            KeyStatus = False
-
 
     spriteRect = pygame.Rect(spriteX, spriteY, sprite.get_width(), sprite.get_height()) #Текущее место расположения игрока
 
@@ -160,26 +145,18 @@ while MainCycle:
         if spriteRectNew.colliderect(barrier[1]):
             (spriteX, spriteY) = oldPos
 
-
     if spriteRectNew.colliderect(interactionRect) and key_pressed[pygame.K_e]:
-        text1 = f1.render('Красный квадрат нажат', 1, (255, 255, 255))
-        i = i + 1
-        print(i)
-        if i % 2 == 0:
-            iteractionFlag = True
-        elif i % 2 != 0:
-            iteractionFlag = False
-
-
-
+        iteractionFlag = True
+    elif spriteRectNew.colliderect(interactionRect) and key_pressed[pygame.K_q]:
+        iteractionFlag = False
+    elif spriteRectNew.colliderect(interactionRect) and iteractionFlag == True:
+        text1 = f1.render('Нажмите Q чтобы выйти', 1, (255, 255, 255))
     elif spriteRectNew.colliderect(interactionRect):
         text1 = f1.render('Нажмите E', 1, (255, 255, 255))
     else:
-        text1 = f1.render('Ничего не происходит', 1, (255, 255, 255))
+        text1 = f1.render('Колизия не обнаружена', 1, (255, 255, 255))
 
-
-
-    OxStatusText = f1.render(str(OxStatus), 1, (255, 255, 255))
+    OxStatusText = f2.render(str(OxStatus), 1, (255, 255, 255))
     if key_pressed[pygame.K_DOWN]:
         OxStatus -= 1
         if OxStatus <= 0:
